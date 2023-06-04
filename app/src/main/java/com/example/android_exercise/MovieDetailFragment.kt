ede19548a1f99fe2de6a6f7d4a502bd2c1ea6975
murmurmuk <tim199980@gmail.com>
2023-06-04 19:15:24 +0800

    detail cover & overview

--- app/src/main/java/com/example/android_exercise/MovieDetailFragment.kt
+++ app/src/main/java/com/example/android_exercise/MovieDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.android_exercise
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -11,38 +12,62 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.android_exercise.data.db.entity.MovieEntry
+import com.example.android_exercise.databinding.FragmentMovieDetailBinding
 import com.example.android_exercise.viewModel.MovieViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class MovieDetailFragment : Fragment() {
-
-
-    val args: MovieDetailFragmentArgs by navArgs()
+    companion object {
+        private const val IMAGE_PREFIX = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2"
+    }
+    private val args: MovieDetailFragmentArgs by navArgs()
 
     private val viewModel by activityViewModels<MovieViewModel>()
+
+    private var _binding: FragmentMovieDetailBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        _binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
+        return _binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d("murmur", "open ${args.movieId}")
         viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
                 viewModel.getMovieInfo(args.movieId)
                     .collect {
-                        Log.d("murmur", "$it")
+                        setMovie(it)
                     }
             }
         }
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    private fun setMovie(movie: MovieEntry) {
+        Log.d("murmur", "$movie")
+        _binding?.apply {
+            val link = if (movie.poster_path.isNullOrEmpty()) {
+                null
+            } else {
+                IMAGE_PREFIX + movie.poster_path
+            }
+            Glide.with(coverImage)
+                .load(Uri.parse(link))
+                .into(coverImage)
+
+            overviewText.text = movie.overview
+        }
     }
 }
