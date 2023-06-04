6f8d3d16ef069f42bc13ccdd9c3600cd2065b5fb
murmurmuk <tim199980@gmail.com>
2023-06-04 18:24:58 +0800

    toolbar

--- app/src/main/java/com/example/android_exercise/MainActivity.kt
+++ app/src/main/java/com/example/android_exercise/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.android_exercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.android_exercise.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.root.post {
+            val controller = findNavController(R.id.nav_host_fragment)
+            val appBarConfiguration = AppBarConfiguration(controller.graph)
+            binding.toolbar.setupWithNavController(controller, appBarConfiguration)
+        }
+
     }
 }
